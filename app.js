802f408a3600dd9da64b8639028c5c3515eb3cbc
axel <axel.bouillart@gmail.com>
2016-10-18 16:44:44 +0200

    upgrade countdown js design pattern

--- app.js
+++ app.js
@@ -1,61 +1,73 @@
+'use strict';
+
 (function(){
 
-	countdown = {
+	var countdown = {
 
 		isPause : false,
-		time : 1500,
+		time : 300,
+		defaultTime : 300,
 		interval : null,
 
 		init: function(){
 
-			countdown.listeners();
+			this.listeners();
 
 		},
 		listeners: function(){
 
-			$('#play').on('click', countdown.play);
-			$('#pause').on('click', countdown.pause);
-			$('#reset').on('click', countdown.reset);
+			$('#play').on('click', this.play.bind(this));
+			$('#pause').on('click', this.pause.bind(this));
+			$('#reset').on('click', this.reset.bind(this));
+			$('#bMinutes').on('click', this.recupM);
+			$('#bSecondes').on('click',this.recupS);
 		},
 
 		play: function(){
 
-			countdown.interval = setInterval(countdown.decrement,1000);
+			this.interval = setInterval(this.decrement.bind(this),1000);
 
 		},
 		pause: function(){
 
-			clearInterval(countdown.interval);
+			clearInterval(this.interval);
 
 		},
 		reset: function(){
 
-			if(countdown.reset) {
-				clearInterval(countdown.time);
-				countdown.time = 1500;
+			if(this.reset) {
+				clearInterval(this.time);
+				this.time = 300;
 			}
 		},
 		decrement:function(){
 
-			countdown.time--;
-			countdown.updateView();
+			this.progressBar();
+			this.time--;
+			this.updateView();
 
-			if(countdown.time === 0){
-				clearInterval(countdown.interval);
+			if(this.time === 0){
+				clearInterval(this.interval.bind(this));
 			}
 
 		},
 		updateView: function(){
 
-			var minute= parseInt(countdown.time/60,10)
-			var seconde= countdown.time % 60;
+			var minute= parseInt(this.time/60,10)
+			var seconde= this.time % 60;
 			$('#minutes').html(minute);
 			$('#secondes').html(seconde);
 
 		},
 		recupInput: function(){
 
-			var recupInput = $('#bMinutes').val();
+			var recupM = $('#inputM').val();
+			var recupS = $('#inputS').val();
+		},
+		progressBar: function(){
+			var progress = parseInt(this.time*100/this.defaultTime,10);
+			$('#chargement').css('width',progress+'%');
+			console.log(progress);
 		}
 	};
 	countdown.init();
